727bebb7e24f0da87052911a68096dff24103385
CrisDaniel25 <63526691+CrisDaniel25@users.noreply.github.com>
2021-06-24 21:01:50 -0400

    [MODIFIED]

--- notebook.py
+++ notebook.py
@@ -1,12 +1,10 @@
-# import pandas as pd
-# Datos = pd.DataFrame( {"a" : [9 ,5, 3], 
-#   "b" : [7, 1, 2], 
-#   "c" : [4, 8, 6]
-#  },
-#  index = [1, 2, 3]
-# )
+from google.colab import files
+uploaded = files.upload()
 
-# df.drop_duplicates()
+import pandas as pd 
+datos_maraton = pd.read_csv('MarathonData.csv')
+datos_maraton
 
-# import files
-# uploaded = files.upload()
+datos_maraton['CrossTraining']
+
+datos_maraton.dropna(subset=["CrossTraining"])
